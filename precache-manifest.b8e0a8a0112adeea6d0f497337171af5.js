a26c081030f90f3a429721ae49800de8956a73bd
Carles Pérez <98perezcarles@gmail.com>
2019-09-03 12:41:27 +0200

    Updates

---
+++ precache-manifest.b8e0a8a0112adeea6d0f497337171af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31422fa66a9fe2ac7fc57136fcdcac3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71dfee8c2727e801423e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4187d53ab7cdc9d8c1a6",
+    "url": "/static/js/2.28518f7d.chunk.js"
+  },
+  {
+    "revision": "71dfee8c2727e801423e",
+    "url": "/static/js/main.5b7e9b90.chunk.js"
+  },
+  {
+    "revision": "ca5e8926572f30f3d7f1",
+    "url": "/static/js/runtime~main.1d57fdf3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
